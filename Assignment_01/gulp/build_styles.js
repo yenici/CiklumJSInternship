b2d32a5d8fc4a64f2404de7bd89c0ddb6ae035cc
Dmytro Novikov <Dmytro.G.Novikov@gmail.com>
2016-09-08 23:41:46 +0300

    A build for production added

--- Assignment_01/gulp/build_styles.js
+++ Assignment_01/gulp/build_styles.js
@@ -10,23 +10,45 @@ var gulp = require('gulp'),
   connect = require('gulp-connect');
 
 module.exports = function() {
-  return gulp.src(path.source.styles + 'main.scss')
-    .pipe(sass().on('error', sass.logError), {
-      outputStyle: 'expanded',
-      indentType: 'space',
-      indentWidth: 2,
-      precision: 10
-    })
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 8'],
-      cascade: true
-    }))
-    .pipe(sourcemaps.init())
-    .pipe(cleancss({compatibility: 'ie8'}))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(path.dist.styles))
-    .pipe(connect.reload());
+  var buf = gulp.src(path.source.styles + 'main.scss');
+  if (process.env.NODE_ENV !== 'production') {
+    return buf
+      .pipe(sass().on('error', sass.logError), {
+        outputStyle: 'expanded',
+        indentType: 'space',
+        indentWidth: 2,
+        precision: 10
+      })
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions', 'ie >= 8'],
+        cascade: true
+      }))
+      .pipe(sourcemaps.init())
+      .pipe(cleancss({compatibility: 'ie8'}))
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(path.dist.styles))
+      .pipe(connect.reload());
+
+  } else {
+    return buf
+      .pipe(sass().on('error', sass.logError), {
+        outputStyle: 'expanded',
+        indentType: 'space',
+        indentWidth: 2,
+        precision: 10
+      })
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions', 'ie >= 8'],
+        cascade: true
+      }))
+      .pipe(cleancss({compatibility: 'ie8'}))
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest(path.dist.styles))
+      .pipe(connect.reload());
+  }
 };
